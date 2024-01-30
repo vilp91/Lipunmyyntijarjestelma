16ccf186050c5d4ae1083fcf8fbe3c20748a7b66
vilp91 <vpajukangas@gmail.com>
2024-01-30 16:50:23 +0200

    Eka commit

---
+++ src/main/java/ohjelmistoprojekti1/a3004/A3004Application.java
@@ -0,0 +1,13 @@
+package ohjelmistoprojekti1.a3004;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A3004Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A3004Application.class, args);
+	}
+
+}
---
+++ src/test/java/ohjelmistoprojekti1/a3004/A3004ApplicationTests.java
@@ -0,0 +1,13 @@
+package ohjelmistoprojekti1.a3004;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A3004ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
